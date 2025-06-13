25aa7185408d5089cbf24e07a579296ed9cc1fe6
shyam-prasad657 <130679445+shyam-prasad657@users.noreply.github.com>
2025-06-13 16:43:02 +0530

    Update carousel.jsx

--- src/pages/carousel.jsx
+++ src/pages/carousel.jsx
@@ -9,7 +9,7 @@ function CarouselComponent() {
     <section className='container-fluid px-0 d-flex flex-column align-items-center' id = 'carousel-container'>
       <div className="container px-0 mx-0 py-5">
         <p className="h2 carousel-text text-center">Clients Who Trust Us</p>
-        <div className="px-0 mt-4"><Marquee gradient = {true} gradientColor = '#F5F5F5'>
+        <div className="px-0 mt-4"><Marquee gradient = {true} gradientColor = '#FFFFFF'>
             <img src = {logo} className='img-fluid ms-4' height= '70px' width='150px'></img>
             <img src = {logo} className='img-fluid ms-4' height= '70px' width='150px'></img>
             <img src = {logo} className='img-fluid ms-4' height= '70px' width='150px'></img>
